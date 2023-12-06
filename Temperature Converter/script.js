1916f64c4bd616a3b07c5adddac9021e6f8bfa55
mbejadiana <99396709+mbejadiana@users.noreply.github.com>
2023-12-06 16:45:36 +0300

    Add files via upload

---
+++ Temperature Converter/script.js
@@ -0,0 +1,26 @@
+const celsius = document.querySelector(".celsius"),
+  fah = document.querySelector(".fahrenheit"),
+  kelvin = document.querySelector(".kelvin"),
+  form = document.querySelector("form");
+  
+form.addEventListener("input", convertTemperature);
+
+function convertTemperature(e){
+  if (e.target.classList.contains("celsius")) {
+    let x = e.target.value;
+    fah.value = x * 1.8 + 32;
+    kelvin.value = x + 273.15;
+  }
+  if (e.target.classList.contains("fahrenheit")) {
+    let x = e.target.value;
+    celsius.value = (x - 32) / 1.8;
+    kelvin.value = (x - 32) / 1.8 + 273.15;
+  }
+  if (e.target.classList.contains("kelvin")) {
+    let x = e.target.value;
+    fah.value = (x - 273.15) * 1.8 + 32;
+    celsius.value = parseFloat(x) - 273.15;
+  }
+  
+}
+
---
+++ To Do List/app.js
@@ -0,0 +1,76 @@
+window.onload = function () {
+  displayTask();
+};
+
+const input = document.querySelector("input"),
+  btn = document.querySelector("button"),
+  todoList = document.querySelector(".todo-list"),
+  clear = document.querySelector(".clear");
+let tasks;
+
+btn.addEventListener("click", addTask);
+
+function getTasks() {
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+  }
+}
+
+function addTask(e) {
+  e.preventDefault();
+  if (input.value !== "") {
+    addTaskToLS();
+    todoList.innerHTML = "";
+    displayTask();
+  } else {
+    alert("Please enter a task");
+  }
+
+}
+
+function addTaskToLS() {
+  getTasks();
+  tasks.push(input.value);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  input.value = "";
+}
+
+function displayTask() {
+  getTasks();
+
+  tasks.forEach((task, index) => {
+    const newLi = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = `<i class="fas fa-trash-alt"id="${index}" onclick="deleteTask(this.id)" ></i>`;
+
+    newLi.appendChild(document.createTextNode(task));
+    newLi.appendChild(delBtn);
+    todoList.appendChild(newLi);
+  });
+}
+
+function deleteTask(index) {
+  const del = confirm("You are about to delete this task!!!")
+  if (del == true) {
+    getTasks();
+  }
+
+  tasks.splice(index, 1);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  todoList.innerHTML = "";
+  displayTask();
+}
+
+clear.addEventListener("click", clearTasks);
+
+function clearTasks() {
+  const delTasks = confirm("Delete all tasks");
+
+  if (delTasks == true) {
+    localStorage.clear();
+    todoList.innerHTML = "";
+    displayTask();
+  }
+}
